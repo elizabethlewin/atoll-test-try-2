8142c03c29d46d14b4257b72313f4051c577ae68
elizabethlewin <37307189+elizabethlewin@users.noreply.github.com>
2018-03-12 16:46:35 -0400

    try2

---
+++ AtollTest/Assets/TriggerPhysicsDrop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPhysicsDrop : MonoBehaviour
+{
+    [SerializeField] private Collider _terrainCollider;
+
+    void OnTriggerEnter(Collider other)
+    {
+        _terrainCollider.enabled = false;
+    }
+}
