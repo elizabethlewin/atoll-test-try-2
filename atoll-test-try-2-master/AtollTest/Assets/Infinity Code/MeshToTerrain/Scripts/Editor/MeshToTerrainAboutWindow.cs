1ab5825f25f79d90400f0dc84f6fcbaf407bc7c2
elizabethlewin <37307189+elizabethlewin@users.noreply.github.com>
2018-03-16 16:50:50 -0400

    changes made
    I messed it up.

---
+++ atoll-test-try-2-master/AtollTest/Assets/Infinity Code/MeshToTerrain/Scripts/Editor/MeshToTerrainAboutWindow.cs
@@ -0,0 +1,31 @@
+﻿/*       INFINITY CODE 2013 - 2017         */
+/*     http://www.infinity-code.com        */
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshToTerrainAboutWindow : EditorWindow
+{
+    [MenuItem("Window/Infinity Code/Mesh to Terrain/About")]
+    public static void OpenWindow()
+    {
+        MeshToTerrainAboutWindow window = GetWindow<MeshToTerrainAboutWindow>(true, "About", true);
+        window.minSize = new Vector2(200, 100);
+        window.maxSize = new Vector2(200, 100);
+    }
+
+    public void OnGUI()
+    {
+        GUIStyle titleStyle = new GUIStyle(EditorStyles.boldLabel);
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+
+        GUIStyle textStyle = new GUIStyle(EditorStyles.label);
+        textStyle.alignment = TextAnchor.MiddleCenter;
+
+        GUILayout.Label("Mesh to Terrain", titleStyle);
+        GUILayout.Label("version " + MeshToTerrain.version, textStyle);
+        GUILayout.Label("created Infinity Code", textStyle);
+        GUILayout.Label("2013-" + DateTime.Now.Year, textStyle);
+    }
+}
